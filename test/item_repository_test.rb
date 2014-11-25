71fe392b7e42713fc613794d04f5763ea8f681c2
Michael Dao <michael.dao@gmail.com>
2014-11-25 11:55:21 -0700

    closes mikedao/sales_engine#32

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -5,7 +5,8 @@ require_relative '../lib/item.rb'
 class ItemRepositoryTest < Minitest::Test
   attr_reader   :data1,
                 :data2,
-                :data3
+                :data3,
+                :item_repository
 
   def setup
     @data1 = {
@@ -24,7 +25,7 @@ class ItemRepositoryTest < Minitest::Test
       description: "Nums",
       unit_price: "11111",
       merchant_id: "2",
-      created_at: "2012-03-28 14:53:59 UTC",
+      created_at: "2012-03-27 14:53:59 UTC",
       updated_at: "2012-03-28 14:53:59 UTC"
     }
 
@@ -35,37 +36,144 @@ class ItemRepositoryTest < Minitest::Test
       unit_price: "22222",
       merchant_id: "2",
       created_at: "2012-03-29 14:53:59 UTC",
-      updated_at: "2012-03-29 14:53:59 UTC"
+      updated_at: "2012-03-28 14:53:59 UTC"
     }
   end
 
-  def test_it_starts_empty
-    itemrepository = ItemRepository.new
-    assert itemrepository.data.empty?
+  def load_test_data
+    @item_repository = ItemRepository.new
+    item_repository << Item.new(data1)
+    item_repository << Item.new(data2)
+    item_repository << Item.new(data3)
   end
-  
+
+  def test_it_starts_empty
+    item_repository = ItemRepository.new
+    assert item_repository.data.empty?
+  end
+
   def test_it_has_items
-    itemrepository = ItemRepository.new
-    itemrepository << Item.new(data1)
-    itemrepository << Item.new(data2)
-    itemrepository << Item.new(data3)
-    refute itemrepository.data.empty?
+    load_test_data
+    refute item_repository.data.empty?
   end
 
   def test_it_has_three_items
-    itemrepository = ItemRepository.new
-    itemrepository << Item.new(data1)
-    itemrepository << Item.new(data2)
-    itemrepository << Item.new(data3)
-    assert_equal 3, itemrepository.data.size
+    load_test_data
+    assert_equal 3, item_repository.data.size
   end
 
   def test_it_can_access_individual_items
-    itemrepository = ItemRepository.new
-    itemrepository << Item.new(data1)
-    itemrepository << Item.new(data2)
-    itemrepository << Item.new(data3)
-    assert_equal 22222, itemrepository.data[2].unit_price
+    load_test_data
+    assert_equal 22222, item_repository.data[2].unit_price
+  end
+
+  def test_all_method_returns_all_items
+    load_test_data
+    refute item_repository.all.empty?
+    assert_equal 3, item_repository.all.size
+    assert_equal item_repository.data, item_repository.all
+  end
+
+  def test_all_returns_array
+    load_test_data
+    assert item_repository.all.is_a?(Array)
+  end
+
+  def test_random_returns_one_item
+    load_test_data
+    assert item_repository.random.is_a?(Item)
+  end
+
+  def test_random_does_not_return_array_of_merchants
+    load_test_data
+    refute item_repository.random.is_a?(Array)
+  end
+
+  def test_find_by_id
+    load_test_data
+    result = item_repository.find_by_id(2)
+    assert_equal "Apple Pie", result.name
+  end
+
+  def test_find_all_by_id
+    load_test_data
+    result = item_repository.find_all_by_id(7)
+    assert_equal "Turkey", result.first.name
+  end
+
+  def test_find_by_name
+    load_test_data
+    result = item_repository.find_by_name("Turkey")
+    assert_equal 7, result.id
+  end
+
+  def test_find_all_by_name
+    load_test_data
+    result = item_repository.find_all_by_name("Skateboard")
+    assert_equal 1, result.first.id
+  end
+
+  def test_find_by_description
+    load_test_data
+    result = item_repository.find_by_description("Food")
+    assert_equal "Turkey", result.name
+  end
+
+  def test_find_all_by_description
+    load_test_data
+    result = item_repository.find_all_by_description("Nums")
+    assert_equal "Apple Pie", result.first.name
+  end
+
+  def test_find_by_unit_price
+    load_test_data
+    result = item_repository.find_by_unit_price(75107)
+    assert_equal "Skateboard", result.name
+  end
+
+  def test_find_all_by_unit_price
+    load_test_data
+    result = item_repository.find_all_by_unit_price(22222)
+    assert_equal "Food", result.last.description
+  end
+
+  def test_find_by_merchant_id
+    load_test_data
+    result = item_repository.find_by_merchant_id(2)
+    assert_equal "Nums", result.description
+  end
+
+  def test_find_all_merchant_id
+    load_test_data
+    result = item_repository.find_all_by_merchant_id(2)
+    assert_equal 2, result.size
+    assert_equal "Turkey", result.last.name
+  end
+
+  def test_find_by_created_at
+    load_test_data
+    result = item_repository.find_by_created_at("2012-03-27 14:53:59 UTC")
+    assert_equal "Skateboard", result.name
+  end
+
+  def test_find_all_by_created_at
+    load_test_data
+    result = item_repository.find_all_by_created_at("2012-03-27 14:53:59 UTC")
+    assert_equal 2, result.size
+    assert_equal "Apple Pie", result.last.name
+  end
+
+  def test_find_by_updated_at
+    load_test_data
+    result = item_repository.find_by_updated_at("2012-03-28 14:53:59 UTC")
+    assert_equal "Apple Pie", result.name
+  end
+
+  def test_find_all_by_updated_at
+    load_test_data
+    result = item_repository.find_all_by_updated_at("2012-03-28 14:53:59 UTC")
+    assert_equal 2, result.size
+    assert_equal 7, result.last.id
   end
 
 end
