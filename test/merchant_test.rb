d60f634c59fc511a6fccddd7ff71e0bc26a25e6f
Michael Dao <michael.dao@gmail.com>
2014-11-24 17:59:22 -0700

    closes mikedao/sales_engine#7

---
+++ test/merchant_test.rb
@@ -0,0 +1,36 @@
+require_relative 'test_helper'
+require_relative '../lib/merchant'
+
+class MerchantTest < Minitest::Test
+  attr_accessor :data
+  #id,name,created_at,updated_at
+  #1,Schroeder-Jerde,2012-03-27 14:53:59 UTC,2012-03-27 14:53:59 UTC
+  def setup
+    @data = { id: "1",
+              name: "Schroeder-Jerde",
+              created_at: "2012-03-27 14:53:59 UTC",
+              updated_at: "2012-03-27 14:53:59 UTC"
+            }
+  end
+
+  def test_it_has_an_id
+    merchant = Merchant.new(data)
+    assert_equal 1, merchant.id
+  end
+
+  def test_it_has_a_name
+    merchant = Merchant.new(data)
+    assert_equal "Schroeder-Jerde", merchant.name
+  end
+
+  def test_it_has_a_created_at
+    merchant = Merchant.new(data)
+    assert_equal "2012-03-27 14:53:59 UTC", merchant.created_at
+  end
+
+  def test_it_has_a_updated_at
+    merchant = Merchant.new(data)
+    assert_equal "2012-03-27 14:53:59 UTC", merchant.updated_at
+  end
+
+end
