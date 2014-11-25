e54e36c12e0da45d7e1f4a13fa019a57b47f72ff
Michael Dao <michael.dao@gmail.com>
2014-11-25 10:45:39 -0700

    implemented find_by

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -8,4 +8,25 @@ class MerchantRepository
   def <<(data)
     @data << data
   end
+
+  def all
+    @data
+  end
+
+  def random
+    @data.sample
+  end
+
+  def find_by_id(criteria)
+    @data.find do |datum|
+      datum.id == criteria
+    end
+  end
+
+  def find_all_by_id(criteria)
+    @data.find_all do |datum|
+      datum.id == criteria
+    end
+  end
+  
 end
