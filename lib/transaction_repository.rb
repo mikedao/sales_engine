930f6c09a4c03bacdb4d386f4b1bb28a4b633a21
Michael Dao <michael.dao@gmail.com>
2014-11-25 16:45:23 -0700

    Added refactored finder

--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,6 +1,7 @@
 class TransactionRepository
   attr_reader :data
 
+
   def initialize
     @data = []
   end
@@ -8,4 +9,33 @@ class TransactionRepository
   def <<(data)
     @data << data
   end
+
+  def all
+    @data
+  end
+
+  def random
+    @data.sample
+  end
+
+  def find_by_id(criteria)
+    finder_by(:id, criteria)
+  end
+
+  def find_all_by_id(criteria)
+    finder_all_by(:id, criteria)
+  end
+
+  def finder_by(attribute, criteria)
+    @data.find do |datum|
+      datum.send(attribute) == criteria
+    end
+  end
+
+  def finder_all_by(attribute, criteria)
+    @data.find_all do |datum|
+      datum.send(attribute) == criteria
+    end
+  end
+
 end
--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -3,7 +3,7 @@ require_relative '../lib/transaction_repository'
 require_relative '../lib/transaction'
 
 class TransactionsRepositoryTest < Minitest::Test
-  attr_reader :data1, :data2, :data3
+  attr_reader :data1, :data2, :data3, :transaction_repository
 
   def setup
     @data1 = { id:                   "1",
@@ -35,25 +35,61 @@ class TransactionsRepositoryTest < Minitest::Test
 
   end
 
+  def load_test_data
+    @transaction_repository = TransactionRepository.new
+    transaction_repository << Transaction.new(data1)
+    transaction_repository << Transaction.new(data2)
+    transaction_repository << Transaction.new(data3)
+  end
+
+
   def test_it_starts_empty
     transaction_repository = TransactionRepository.new
     assert transaction_repository.data.empty?
   end
 
   def test_it_has_transactions
-    transaction_repository = TransactionRepository.new
-    transaction_repository << Transaction.new(data1)
-    transaction_repository << Transaction.new(data2)
-    transaction_repository << Transaction.new(data3)
+    load_test_data
     assert_equal 3, transaction_repository.data.size
   end
 
   def test_it_can_access_individual_transactions
-    transaction_repository = TransactionRepository.new
-    transaction_repository << Transaction.new(data1)
-    transaction_repository << Transaction.new(data2)
-    transaction_repository << Transaction.new(data3)
+    load_test_data
     assert_equal "failed", transaction_repository.data[2].result
+  end
+
+  def test_all_method_returns_all_transactions
+    load_test_data
+    refute transaction_repository.all.empty?
+    assert_equal 3, transaction_repository.all.size
+    assert_equal transaction_repository.data, transaction_repository.all
+  end
+
+  def test_all_returns_an_array
+    load_test_data
+    assert transaction_repository.all.is_a?(Array)
+  end
+
+  def test_random_returns_one_transaction
+    load_test_data
+    assert transaction_repository.random.is_a?(Transaction)
+  end
+
+  def test_random_does_not_return_array_of_transactions
+    load_test_data
+    refute transaction_repository.random.is_a?(Array)
+  end
+
+  def test_find_by_id
+    load_test_data
+    result = transaction_repository.find_by_id(2)
+    assert_equal 4654405418242222, result.credit_card_number
+  end
+
+  def test_find_all_by_id
+    load_test_data
+    result = transaction_repository.find_all_by_id(2)
+    assert_equal 4654405418242222, result.first.credit_card_number
   end
 
 end
