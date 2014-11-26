a57ae6a162622de3e62773a493ec23c248775ca7
Rich Shea <richcshea@gmail.com>
2014-11-25 18:34:01 -0700

    closes mikedao/sales_engine#61

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -18,52 +18,47 @@ class MerchantRepository
   end
 
   def find_by_id(criteria)
-    @data.find do |datum|
-      datum.id == criteria
-    end
+    finder_by(:id, criteria)
   end
 
   def find_all_by_id(criteria)
-    @data.find_all do |datum|
-      datum.id == criteria
-    end
+    finder_all_by(:id, criteria)
   end
 
   def find_by_name(criteria)
-    @data.find do |datum|
-      datum.name == criteria
-    end
+    finder_by(:name, criteria)
   end
 
   def find_all_by_name(criteria)
-    @data.find_all do |datum|
-      datum.name == criteria
-    end
+    finder_all_by(:name, criteria)
   end
 
   def find_by_created_at(criteria)
-    @data.find do |datum|
-      datum.created_at == criteria
-    end
+    finder_by(:created_at, criteria)
   end
 
   def find_all_by_created_at(criteria)
-    @data.find_all do |datum|
-      datum.created_at == criteria
-    end
+    finder_all_by(:created_at, criteria)
   end
 
   def find_by_updated_at(criteria)
-    @data.find do |datum|
-      datum.updated_at == criteria
-    end
+    finder_by(:updated_at, criteria)
   end
 
   def find_all_by_updated_at(criteria)
-    @data.find_all do |datum|
-      datum.updated_at == criteria
+    finder_all_by(:updated_at, criteria)
+  end
+
+  def finder_by(attribute, criteria)
+    @data.find do |datum|
+      datum.send(attribute) == criteria
     end
   end
 
+  def finder_all_by(attribute, criteria)
+    @data.find_all do |datum|
+      datum.send(attribute) == criteria
+    end
+  end
 
 end
