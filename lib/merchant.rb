77210cc731464d51d96131a144b4022ede331268
Rich Shea <richcshea@gmail.com>
2014-11-26 13:52:19 -0700

    closes mikedao/sales_engine#73"

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -2,13 +2,15 @@ class Merchant
   attr_reader   :id,
                 :name,
                 :created_at,
-                :updated_at
+                :updated_at,
+                :repository
 
-  def initialize(data)
-    @id = data[:id].to_i
-    @name = data[:name]
+  def initialize(data, parent)
+    @id         = data[:id].to_i
+    @name       = data[:name]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
+    @repository = parent
   end
 
 end
--- test/invoice_test.rb
+++ test/invoice_test.rb
@@ -18,37 +18,37 @@ class InvoiceTest < Minitest::Test
   end
 
   def test_it_has_an_id
-    invoice = Invoice.new(data, self)
+    invoice = Invoice.new(data, nil)
 
     assert_equal 1, invoice.id
   end
 
   def test_it_has_a_customer_id
-    invoice = Invoice.new(data)
+    invoice = Invoice.new(data, nil)
 
     assert_equal 1, invoice.customer_id
   end
 
   def test_it_has_a_merchant_id
-    invoice = Invoice.new(data)
+    invoice = Invoice.new(data, nil)
 
     assert_equal 26, invoice.merchant_id
   end
 
   def test_it_has_a_status
-    invoice = Invoice.new(data)
+    invoice = Invoice.new(data, nil)
 
     assert_equal "shipped", invoice.status
   end
 
   def test_it_has_a_created_at_date
-    invoice = Invoice.new(data)
+    invoice = Invoice.new(data, nil)
 
     assert_equal "2012-03-25 09:54:09 UTC", invoice.created_at
   end
 
   def test_it_has_an_updated_at_date
-    invoice = Invoice.new(data)
+    invoice = Invoice.new(data, nil)
 
     assert_equal "2012-03-25 09:54:09 UTC", invoice.updated_at
   end
