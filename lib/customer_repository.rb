b918ee921421d2db2cf7f3be53aa2f670ccacfa9
Michael Dao <michael.dao@gmail.com>
2014-11-25 17:20:29 -0700

    closes mikedao/sales_engine#35

--- lib/customer_repository.rb
+++ lib/customer_repository.rb
@@ -8,4 +8,41 @@ class CustomerRepository
   def <<(data)
     @data << data
   end
+
+  def all
+    @data
+  end
+
+  def random
+    @data.sample
+  end
+
+  def find_by_id(criteria)
+    finder_by(:id, criteria)
+  end
+
+  def find_all_by_id(criteria)
+    finder_all_by(:id, criteria)
+  end
+
+  def find_by_first_name(criteria)
+    finder_by(:first_name, criteria)
+  end
+
+  def find_all_by_first_name(criteria)
+    finder_by(:first_name, criteria)
+
+  def finder_by(attribute, criteria)
+    @data.find do |datum|
+      datum.send(attribute) == criteria
+    end
+  end
+
+  def finder_all_by(attribute, criteria)
+    @data.find_all do |datum|
+      datum.send(attribute) == criteria
+    end
+  end
+
+
 end
